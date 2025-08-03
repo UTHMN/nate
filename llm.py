55e95ebee5b59f947921843f46cb636ce677d6ee
Uthman Fatih <kingvcheese@gmail.com>
2025-08-03 19:59:10 +0100

    Broke into api (cli work soon)

---
+++ llm.py
@@ -0,0 +1,85 @@
+# LLM
+from ollama import chat, ChatResponse
+
+# Memory
+from json import loads, dumps
+from os.path import exists
+
+# .env
+from dotenv import load_dotenv
+from os.path import dirname, join
+from os import getenv
+
+class LLM:
+    def __init__(self) -> None:
+        dotenv_path = join(dirname(__file__), ".env")
+        load_dotenv(dotenv_path)
+        
+        # Initialize variables
+        self.model = getenv("model")
+        if self.model is None:
+            self.model = "gemma3:1b-it-qat"
+        
+        self.messages = self.memory_load()
+    
+    def ask(self, prompt: str) -> str:
+        ''' Prompts an AI based on memory '''
+        
+        # save user input to memory
+        self.messages.append({
+            "role": "user",
+            "content": prompt
+        })
+        
+        response = chat(model=self.model, messages=self.messages)
+        
+        # save assistant output to memory
+        self.messages.append({
+            "role": "assistant",
+            "content": response['message']['content']
+        })
+        
+        # save memory json to file
+        self.memory_append()
+        
+        return response['message']['content']
+    
+    def prompt(self, prompt: str) -> str:
+        ''' Accepts an input string and returns the response as a string. '''
+        
+        response = chat(model=self.model, messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ])
+        
+        return(response['message']['content'])
+    
+    def memory_append(self) -> None:
+        ''' Appends to memory.json file '''
+        
+        path = join(dirname(__file__), "messages.json")
+        
+        if not exists(path): return
+        
+        with open(path, "w") as f:
+            f.write(dumps(self.messages, indent=4))
+            
+    def memory_load(self) -> str:
+        ''' Loads memory from the memory.json file '''
+        
+        path = join(dirname(__file__), "messages.json")
+        
+        if not exists(path):
+            with open(path, "w") as f:
+                f.write("[]")
+        
+            return []
+        
+        with open(path, "r") as f:
+            data = loads(f.read())
+            return data
+    
+    # def memory_compress(self)
+    # Uses self.prompt to summarise all memory until previous call
--- main.py
+++ main.py
@@ -1,94 +1,25 @@
-# LLM
-from ollama import chat, ChatResponse
+# Fastapi
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-# Memory
-from json import loads, dumps
-from os.path import exists
+# llm
+from llm import LLM
 
-# .env
-from dotenv import load_dotenv
-from os.path import dirname, join
-from os import getenv
+app = FastAPI()
+model = LLM()
 
-dotenv_path = join(dirname(__file__), ".env")
-load_dotenv(dotenv_path)
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-class LLM:
-    def __init__(self) -> None:
-        # Initialize variables
-        self.model = getenv("model")
-        self.messages = self.memory_load()
-    
-    def ask(self, prompt: str) -> str:
-        ''' Prompts an AI based on memory '''
-        
-        # save user input to memory
-        self.messages.append({
-            "role": "user",
-            "content": prompt
-        })
-        
-        response = chat(model=self.model, messages=self.messages)
-        
-        # save assistant output to memory
-        self.messages.append({
-            "role": "assistant",
-            "content": response['message']['content']
-        })
-        
-        # save memory json to file
-        self.memory_append()
-        
-        return response['message']['content']
-    
-    def prompt(self, prompt: str) -> str:
-        ''' Accepts an input string and returns the response as a string. '''
-        
-        response = chat(model=self.model, messages=[
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ])
-        
-        return(response['message']['content'])
-    
-    def memory_append(self) -> None:
-        ''' Appends to memory.json file '''
-        
-        path = join(dirname(__file__), "messages.json")
-        
-        if not exists(path): return
-        
-        with open(path, "w") as f:
-            f.write(dumps(self.messages, indent=4))
-            
-    def memory_load(self) -> str:
-        ''' Loads memory from the memory.json file '''
-        
-        path = join(dirname(__file__), "messages.json")
-        
-        if not exists(path):
-            with open(path, "w") as f:
-                f.write("[]")
-        
-            return []
-        
-        with open(path, "r") as f:
-            data = loads(f.read())
-            return data
-    
-    # def memory_compress(self)
-    # Uses self.prompt to summarise all memory until previous call
+# Define a request model
+class PromptRequest(BaseModel):
+    prompt: str
+
+@app.post("/ask")
+async def ask(request: PromptRequest):
+    return {"message": model.ask(request.prompt)}
 
 if __name__ == "__main__":
-    model = LLM()
-    
-    while True:
-        try:
-            prompt = input("user> ")
-            print(f"nate> {model.ask(prompt)}")
-        
-        except KeyboardInterrupt:
-            print("Goodbye 👋...")
-            exit(0)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
